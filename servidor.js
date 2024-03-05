6b188e0d04ab2946a2405a882a97822d782f832d
ijoaog <139797591+joao-lopes-axon@users.noreply.github.com>
2024-03-05 09:31:22 -0300

    atts

--- servidor.js
+++ servidor.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3050; // Escolha a porta desejada, por exemplo, 3000
+const port = 4001;
 
 app.use(express.static('public'));
 

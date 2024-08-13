077e696a60a1fc14729b25f1243dba5b497e3847
João Gabriel Klein <66679956+ijoaog@users.noreply.github.com>
2024-08-13 15:29:22 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 4001;
 
 app.use(express.static('public'));
+
+app.get('/gramadaremos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'gramadaremos', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`);
